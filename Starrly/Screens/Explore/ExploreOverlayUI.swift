cf404a0b6ce43af8816cbb382f65de6194103ea4
Vitha <emailvitha@gmail.com>
2026-09-18 09:03:22 +0800

    lil fixie

--- Starrly/Screens/Explore/ExploreOverlayUI.swift
+++ Starrly/Screens/Explore/ExploreOverlayUI.swift
@@ -20,10 +20,12 @@ struct ExploreOverlayUI: View {
 
                 Button(action: onDiscover) {
                     Image(systemName: "plus")
+                        .font(.system(size: 19, weight: .regular))
                         .foregroundStyle(Color.starrlyOffWhite)
+                        .frame(width: 48, height: 48)
+                        .contentShape(Circle())
                 }
                 .buttonStyle(.plain)
-                .padding(10)
                 .glassEffect(.starrly.interactive(), in: .circle)
             }
 
